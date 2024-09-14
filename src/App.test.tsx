d07de60d6c0b8d486e0957fb6c949fd92c187181
shakti97 <shaktisingh230797@gmail.com>
2024-09-14 18:10:47 +0530

    Added test case for updating the input field

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,32 +1,40 @@
-import { render, screen } from "@testing-library/react";
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
 describe("String Calculator Component", () => {
   it("should render the component", () => {
-    render(<App/>);
+    render(<App />);
     expect(screen.getByText("String Calculator")).toBeInTheDocument();
   });
 
   it("should render input box", () => {
-    render(<App/>);
+    render(<App />);
     expect(screen.getByText("String Calculator")).toBeInTheDocument();
   });
 
-  it('renders input field with correct placeholder', () => {
+  it("renders input field with correct placeholder", () => {
     render(<App />);
     const inputElement = screen.getByTestId("input");
     expect(inputElement).toBeInTheDocument();
   });
 
-  it('renders calculate button', () => {
+  it("renders calculate button", () => {
     render(<App />);
     const buttonElement = screen.getByTestId("calculate");
     expect(buttonElement).toBeInTheDocument();
   });
 
-  it('renders result block', () => {
+  it("renders result block", () => {
     render(<App />);
     const resultElement = screen.getByTestId("result");
     expect(resultElement).toBeInTheDocument();
   });
+
+  test("updates input value once user start updating", () => {
+    render(<App />);
+    const inputElement = screen.getByTestId("input");
+    fireEvent.change(inputElement, { target: { value: "1,2,3" } });
+    expect(inputElement).toHaveValue("1,2,3");
+  });
+
 });
