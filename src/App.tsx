66ef28c2f82d74a7685f214df6619839393f6dc7
shakti97 <shaktisingh230797@gmail.com>
2024-09-14 17:35:38 +0530

    Added Simple UI element for component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <h1>String Calculator</h1>
+      <input type="text" data-testid="input" placeholder="Enter numbers" />
+      <button data-testid="calculate">Calculate</button>
+      <p data-testid="result">Result: </p>
+    </>
+  );
 }
 
 export default App;
