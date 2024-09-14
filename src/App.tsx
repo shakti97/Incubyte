1147f84d57d2235b9e217d51912389171c063f60
shakti97 <shaktisingh230797@gmail.com>
2024-09-14 18:17:15 +0530

    Added handling code for button and calling add function

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import { add } from "./utils/Add";
 
 function App(): JSX.Element {
   const [input, setInput] = useState<string>("");
+  const [result, setResult] = useState<number | string>(0);
 
+  const handleCalculate = () => {
+    try {
+      const res = add(input);
+      setResult(res);
+    } catch (error) {
+      setResult(error.message);
+    }
+  };
   return (
     <>
       <h1>String Calculator</h1>
@@ -14,8 +24,10 @@ function App(): JSX.Element {
         placeholder="Enter numbers"
         onChange={(e) => setInput(e.target.value)}
       />
-      <button data-testid="calculate">Calculate</button>
-      <p data-testid="result">Result: </p>
+      <button onClick={handleCalculate} data-testid="calculate">
+        Calculate
+      </button>
+      <p data-testid="result">Result: {result} </p>
     </>
   );
 }
