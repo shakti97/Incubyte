166c0da44f1a26f575b2031b521b8fed93063e8b
shakti97 <shaktisingh230797@gmail.com>
2024-09-14 09:38:16 +0530

    Added scenario which will return the same number if single number is passed

--- src/utils/Add.test.tsx
+++ src/utils/Add.test.tsx
@@ -4,4 +4,8 @@ describe("Add function", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  it('should return the number itself when a single number is passed', () => {
+    expect(add('5')).toBe(5);
+  });
 });
--- src/utils/Add.ts
+++ src/utils/Add.ts
@@ -2,7 +2,9 @@ function add(input: string): number {
   if (input === "") {
     return 0;
   }
-  return 0;
+  if (input.length === 1 && !isNaN(Number(input))) {
+    return Number(input);
+  }
 }
 
 export { add };
