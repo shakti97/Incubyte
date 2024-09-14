cdd7f9e9ca2c2625243846b66f97d7a67b5cf4d8
shakti97 <shaktisingh230797@gmail.com>
2024-09-14 12:45:45 +0530

    Added custom delimeter

--- src/utils/Add.test.tsx
+++ src/utils/Add.test.tsx
@@ -5,15 +5,20 @@ describe("Add function", () => {
     expect(add("")).toBe(0);
   });
 
-  it('should return the number itself when a single number is passed', () => {
-    expect(add('5')).toBe(5);
+  it("should return the number itself when a single number is passed", () => {
+    expect(add("5")).toBe(5);
   });
 
-  it('should return the sum of n numbers separated by a comma', () => {
-    expect(add('1,2,4')).toBe(7);
+  it("should return the sum of n numbers separated by a comma", () => {
+    expect(add("1,2,4")).toBe(7);
   });
 
-  it('should return the sum of n numbers separated by a comma & new line character', () => {
+  it("should return the sum of n numbers separated by a comma & new line character", () => {
     expect(add("1\n2,3")).toBe(6);
+  });
+
+  it("should handle custom delimiters", () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//:\n1:2:3")).toBe(6);
   });
 });
--- src/utils/Add.ts
+++ src/utils/Add.ts
@@ -1,12 +1,23 @@
 function add(input: string): number {
-  // delimiter is a regular expression that matches a comma or a new line character
-  const delimiter = /,|\n/;
-  // split the input string by the delimiter and convert each element to a number
-  const numArray = input.split(delimiter).map(Number);
-  console.log('numArray', numArray);
   if (input === "") {
     return 0;
   }
+
+  // default delimiter is a regular expression that matches a comma or a new line character
+  let delimiter = /,|\n/;
+
+  if(input.startsWith('//')) {
+    // extract the delimiter from the input string
+    const delimiterSection = input.match(/^\/\/(.*)\n/);
+    if(delimiterSection !== null) {
+      delimiter = new RegExp(delimiterSection[1]);
+      input=input.slice(delimiterSection[0].length);
+    }
+  }
+
+  // split the input string by the delimiter and convert each element to a number
+  const numArray = input.split(delimiter).map(Number);
+  console.log('numArray', numArray);
   // return the sum of the numbers in the array using the reduce method
   return numArray.reduce((sum, num) => sum + num, 0);
 }
