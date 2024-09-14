4fd629df1c071720799cd87b1a418ed4076e45c6
shakti97 <shaktisingh230797@gmail.com>
2024-09-14 18:13:17 +0530

    Added updating input field to fix failing test cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {
+function App(): JSX.Element {
+  const [input, setInput] = useState<string>("");
+
   return (
     <>
       <h1>String Calculator</h1>
-      <input type="text" data-testid="input" placeholder="Enter numbers" />
+      <input
+        type="text"
+        value={input}
+        data-testid="input"
+        placeholder="Enter numbers"
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button data-testid="calculate">Calculate</button>
       <p data-testid="result">Result: </p>
     </>
